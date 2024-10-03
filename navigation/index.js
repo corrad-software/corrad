a55a680e0801c959e1c74da2cf93e9d4882b59ce
Md Afiq Iskandar <mdafiqiskandar@gmail.com>
2024-10-03 19:38:51 +0800

    Remove devtools

--- navigation/index.js
+++ navigation/index.js
@@ -13,72 +13,6 @@ export default [
     ],
   },
   {
-    header: "Administration",
-    description: "Manage your application",
-    child: [
-      {
-        title: "Configuration",
-        icon: "ic:outline-settings",
-        child: [
-          {
-            title: "Environment",
-            path: "/devtool/config/environment",
-          },
-        ],
-      },
-      {
-        title: "Menu Editor",
-        icon: "ci:menu-alt-03",
-        path: "/devtool/menu-editor",
-        child: [],
-      },
-      {
-        title: "Manage Users",
-        path: "/devtool/user-management",
-        icon: "ph:user-circle-gear",
-        child: [
-          {
-            title: "User List",
-            path: "/devtool/user-management/user-list",
-            icon: "",
-            child: [],
-          },
-          {
-            title: "Role List",
-            path: "/devtool/user-management/role-list",
-            icon: "",
-            child: [],
-          },
-        ],
-      },
-      {
-        title: "Content",
-        icon: "mdi:pencil-ruler",
-        child: [
-          {
-            title: "Editor",
-            path: "/devtool/content-editor",
-          },
-          {
-            title: "Template",
-            path: "/devtool/content-editor/template",
-          },
-        ],
-      },
-      {
-        title: "API Editor",
-        path: "/devtool/api-editor",
-        icon: "material-symbols:api-rounded",
-        child: [],
-      },
-    ],
-    meta: {
-      auth: {
-        role: ["Developer"],
-      },
-    },
-  },
-  {
     header: "AI in SDLC",
     description: "Software Development",
     child: [
@@ -89,28 +23,5 @@ export default [
         external: true,
       },
     ],
-  },
-  {
-    header: "Help",
-    description: "Help and documentation",
-    child: [
-      {
-        title: "Documentation",
-        icon: "solar:book-bookmark-minimalistic-bold",
-        path: "https://mawar-cms-docs.vercel.app",
-        external: true,
-      },
-      {
-        title: "UI Components",
-        icon: "material-symbols:settings-input-component-outline-rounded",
-        path: "https://corradui.datasc.dev",
-        external: true,
-      },
-    ],
-    meta: {
-      auth: {
-        role: ["Developer"],
-      },
-    },
   },
 ];
